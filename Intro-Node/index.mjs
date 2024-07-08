456acb53adc64e9b25561a15e816b032b86f3a1b
Audrija <95099812+Audi9602@users.noreply.github.com>
2024-07-08 15:21:48 +0530

    Add files via upload

---
+++ Intro-Node/index.mjs
@@ -0,0 +1,78 @@
+//jshint esversion:6
+//use .mjs instead of .js to run npm packages with node on the terminal!
+//install more npm packages by using changing "name"
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Supervillains
+import supervillains from 'supervillains';
+supervillains;
+
+import {randomSupervillain} from 'supervillains';
+console.log(randomSupervillain());
+
+*/
+/* SuperHeroes
+import superheroes from 'superheroes';
+
+superheroes;
+
+import {randomSuperhero} from 'superheroes';
+
+console.log(randomSuperhero());
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* File Module
+const fs = require("fs");
+fs.copyFileSync("testfile.txt","copy.txt");
+*/
