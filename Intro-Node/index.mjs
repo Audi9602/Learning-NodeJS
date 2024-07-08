ec929c7aa3d35a95d34825c830d5a2e847ae9b71
Audrija <95099812+Audi9602@users.noreply.github.com>
2024-07-08 15:22:46 +0530

    Update index.mjs

--- Intro-Node/index.mjs
+++ Intro-Node/index.mjs
@@ -2,26 +2,14 @@
 //use .mjs instead of .js to run npm packages with node on the terminal!
 //install more npm packages by using changing "name"
 
-
-
-
-
-
-
-
-
-
-
-
-
 /* Supervillains
 import supervillains from 'supervillains';
 supervillains;
 
 import {randomSupervillain} from 'supervillains';
 console.log(randomSupervillain());
-
 */
+console.log("Uncomment to use the commands on terminal and editor");
 /* SuperHeroes
 import superheroes from 'superheroes';
 
